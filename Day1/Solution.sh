c54be7f6eb855594f6f8381cf1ef14bc37c969e8
Rutuja chandurkar <rutujachandurkar96@gmail.com>
2024-10-18 11:53:31 +0530

    Create Solution.sh

---
+++ Day1/Solution.sh
@@ -0,0 +1,11 @@
+Day 1- Introduction of DevOps
+
+What is DevOps?
+DevOps is a mindset or culture or methodology that used for automation and combine Development and operations.
+What is Automation, Scaling, and Infrastructure:
+Automation is something that helps increase productivity by targeting repetitive tasks that are done daily, reducing human errors and the need for manual intervention.
+Scaling: Adjusting the system’s capacity automatically based on demand.
+Infrastructure: The physical or virtual resources required to establish operations, which include servers, networks, storage, etc.
+Why DevOps is Important:
+DevOps speeds delivery of higher quality software by combining and automating the work of development and operaton
+Devops Tools used are Git/Github, Docker, Jenkins, Terraform, Ansible, Cloud(AWS), Kubernetes etc.,
